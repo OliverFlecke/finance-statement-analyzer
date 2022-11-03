8b65cb827b6a14db9bcb78e07b29ed923bd80bfb
Oliver Fleckenstein <oliverfl@live.dk>
2022-11-03 22:56:42 +0000

    feat: Building tree in rust to show categories

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+use std::{cell::RefCell, collections::HashMap, str::Split};
+
 use derive_getters::Getters;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Getters)]
+#[derive(Debug, Deserialize, Serialize, Getters, Clone)]
 pub struct Record {
     #[serde(rename = "Transaction Date")]
     date: String,
@@ -27,5 +29,85 @@ impl Record {
 
     pub fn set_category(&mut self, category: String) {
         self.category = Some(category);
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Tree {
+    root: RefCell<Node>,
+}
+
+impl Tree {
+    pub fn insert(&self, record: Record) {
+        Node::insert(&self.root, record);
+    }
+
+    pub fn preorder<F>(&self, action: F)
+    where
+        F: Fn(&RefCell<Node>, usize) + Copy,
+    {
+        self.root
+            .borrow()
+            .children
+            .values()
+            .into_iter()
+            .for_each(|n| Node::preorder(n, action, 0));
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Node {
+    category: String,
+    // children: Vec<RefCell<Node>>,
+    children: HashMap<String, RefCell<Node>>,
+    items: Vec<Record>,
+}
+
+impl Node {
+    pub fn catogory(&self) -> &String {
+        &self.category
+    }
+
+    fn new(category: String) -> Self {
+        Node {
+            category,
+            children: HashMap::default(),
+            items: Vec::default(),
+        }
+    }
+
+    fn insert(root: &RefCell<Node>, record: Record) {
+        fn helper(node: &RefCell<Node>, record: Record, mut splits: Split<char>) {
+            if let Some(cat) = splits.next() {
+                let mut node = node.borrow_mut();
+                let child = node
+                    .children
+                    .entry(cat.to_string())
+                    .or_insert_with(|| RefCell::new(Node::new(cat.to_string())));
+
+                helper(child, record, splits);
+            } else {
+                // Insert as child if this is leaf category
+                node.borrow_mut().items.push(record);
+            }
+        }
+
+        if let Some(category) = record.category().clone() {
+            helper(root, record, category.split('/'));
+        } else {
+            root.borrow_mut().items.push(record);
+        }
+    }
+
+    fn preorder<F>(root: &RefCell<Node>, action: F, depth: usize)
+    where
+        F: Fn(&RefCell<Node>, usize) + Copy,
+    {
+        action(root, depth);
+        root.borrow()
+            .children
+            .values()
+            .into_iter()
+            .for_each(|n| Self::preorder(n, action, depth + 1));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
 
 use clap::Parser;
 use colored::Colorize;
-use finance_analyzer::Record;
+use finance_analyzer::{Record, Tree};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -16,7 +16,7 @@ struct Args {
     #[arg(short, long)]
     output: String,
     #[arg(short, long)]
-    lookup: String,
+    lookup: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -25,8 +25,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut reader = csv::Reader::from_path(args.filename)?;
     let mut writer = csv::Writer::from_path(args.output)?;
 
-    let mut lookup: HashMap<String, String> =
-        serde_json::from_reader(File::open(args.lookup.to_owned())?)?;
+    let mut lookup: HashMap<String, String> = get_initial_lookup(args.lookup.clone());
+
+    let tree = Tree::default();
 
     for result in reader.deserialize() {
         let mut record: Record = result?;
@@ -37,14 +38,32 @@ fn main() -> Result<(), Box<dyn Error>> {
             lookup.insert(record.description().to_owned(), category.to_owned());
         }
 
-        writer.serialize(record)?;
+        writer.serialize(record.clone())?;
+
+        // Tree
+        tree.insert(record);
     }
 
     writer.flush()?;
 
-    fs::write(args.lookup, serde_json::to_string_pretty(&lookup)?)?;
+    if let Some(lookup) = args.lookup {
+        fs::write(&lookup, serde_json::to_string_pretty(&lookup)?)?;
+    }
+
+    tree.preorder(|n, depth| {
+        println!("{:<1$}{category}", "", 4 * depth, category = n.borrow().catogory());
+    });
 
     Ok(())
+}
+
+/// Get the initial lookup `HashMap` stored in the given file.
+/// If the file does not exist or no filename is provided, an empty map is returned.
+fn get_initial_lookup(filename: Option<String>) -> HashMap<String, String> {
+    filename
+        .and_then(|f| File::open(f).ok())
+        .and_then(|file| serde_json::from_reader(file).ok())
+        .unwrap_or_default()
 }
 
 /// Get the catogory, either from the lookup or by asking the user.
